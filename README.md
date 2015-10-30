19fb4b431d918cf89310342cdcc9160ca6eb850a
Edoardo Negri <edne@gmx.com>
2015-10-31 00:38:50 +0100

    removed files

--- README.md
+++ README.md
@@ -1,16 +1,1 @@
-files:
-```
-/etc/systemd/system/b3d.service
-/usr/local/bin/b3d
-/etc/b3d.json
-```
-
-deploy:
-```
-make install
-```
-
-read the logs:
-```
-journalctl -u b3d
-```
+# b3d
--- b3d.py
+++ b3d.py
@@ -1,28 +1,52 @@
 #!/usr/bin/env python3
-from sys import argv
+import websocket
+import json
+import time
+from threading import Thread
+
+data = {}
 
 
-def print_help():
-    print("")
+def on_message(ws, message):
+    diff = json.loads(message)
+    data.update(diff)
+
+    print(diff)
+
+    # TODO dispatching
 
 
-def start():
-    pass
+def on_error(ws, error):
+    print("Error: " + error.message)
+    print("Reconnecting...")
+    start_websocket()
 
 
-def stop():
-    pass
+def on_close(ws):
+    print("Connection closed")
 
 
-def main():
-    if len(argv) != 2:
-        print_help()
-    else:
-        if argv[1] == "start":
-            start()
-        elif argv[1] == "stop":
-            stop()
-        elif argv[1] == "reload":
-            start()
-        else:
-            print_help()
+def on_open(ws):
+    def ping():
+        # ws.recv()
+        ws.send(json.dumps({"test": 123, "key": "antani"}))
+        while 1:
+            # TODO
+            # plugin handling, passing them data
+            # and running them in an other thread
+            ws.send("{}")
+            time.sleep(15)
+
+    Thread(target=ping).start()
+
+
+def start_websocket():
+    ws = websocket.WebSocketApp("ws://localhost:8080",
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close,
+                                on_open=on_open)
+    ws.run_forever()
+
+if __name__ == "__main__":
+    start_websocket()
